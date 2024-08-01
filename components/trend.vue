5c4b3632fbb695b5af4d631d1f8250cb4aefff27
Oleksandr Lysenko <alexsmith.ua@gmail.com>
2024-08-02 02:30:04 +0300

    Add logic with period

--- components/trend.vue
+++ components/trend.vue
@@ -30,6 +30,8 @@ const props = defineProps({
   loading: Boolean
 })
 
+const { amount } = toRefs(props)
+
 const trendingUp = computed(
   () => props.amount >= props.lastAmount
 )
@@ -38,7 +40,7 @@ const icon = computed(
   () => trendingUp.value ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-arrow-trending-down'
 )
 
-const { currency } = useCurrency(props.amount)
+const { currency } = useCurrency(amount)
 
 const percentageTrend = computed(() => {
   if(props.amount === 0 || props.lastAmount === 0) return '∞%'
